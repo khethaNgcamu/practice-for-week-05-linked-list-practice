182859d1cc4f810e55ecea96f41e99aa57e0558f
Smanga Ngcamu <ngcamukhetha@gamil.com>
2024-05-20 11:32:58 +0200

    doubly link list completed

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -9,14 +9,40 @@ class DoublyLinkedListNode {
 class DoublyLinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
     // Your code here
+    const newHeadNode = new DoublyLinkedListNode(val);
+
+    if(!this.head){
+      this.head = newHeadNode;
+      this.tail = newHeadNode;
+    }else{
+      newHeadNode.next = this.head;
+      this.head.prev = newHeadNode;
+      this.head = newHeadNode; 
+    }
+    this.length++;
   }
 
   addToTail(val) {
     // Your code here
+
+    const tailNode = new DoublyLinkedListNode(val);
+
+    if(!this.head){
+      this.head = tailNode;
+      this.tail = tailNode;
+    }else {
+      tailNode.prev = this.tail;
+      this.tail.next = tailNode;
+      this.tail = tailNode;
+    }
+    this.length++;
   }
 
   // You can use this function to help debug
