5543a26d177d994e7199173207e6cbdf581dfd1e
Smanga Ngcamu <ngcamukhetha@gamil.com>
2024-05-20 10:49:37 +0200

    simple linked list completed

--- linked-list.js
+++ linked-list.js
@@ -8,15 +8,37 @@ class LinkedListNode {
 class LinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.length = 0;
   }
 
   addToHead(val) {
     // Your code here
+    const newNode = new LinkedListNode(val);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
   }
 
   addToTail(val) {
-    // Your code here
-  }
+    
+    const newTailNode = new LinkedListNode(val);
+  
+    if (!this.head) {
+      this.head = newTailNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newTailNode;
+    }
+    
+    this.length++; 
+      
+ }
+ 
+
 
   // You can use this function to help debug
   print() {
